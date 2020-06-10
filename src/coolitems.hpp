4c6b0b873b2708b65b2e5cce740925872761479f
TheOnlyHaze <keanuk1303@gmail.com>
2020-06-10 12:56:53 +0200

    Initial commit

---
+++ src/coolitems.hpp
@@ -0,0 +1,29 @@
+std::unordered_map<std::string, std::string> coolitems = {
+
+//Valuables
+{ "5733279d245977289b77ec24", "Battery"},
+{ "590a373286f774287540368b", "Dfuel"},
+{ "590c657e86f77412b013051d", "Grizzly"},
+{ "5d03794386f77420415576f5", "Tank battery" },
+{ "5d0379a886f77420407aa271", "OFZ " },
+{ "5d1b36a186f7742523398433", "Fuel" },
+{ "5d1b371186f774253763a656", "Fuel" },
+{ "5c12688486f77426843c7d32", "Paracord" },
+{ "573478bc24597738002c6175", "Horse" },
+{ "59e3658a86f7741776641ac4", "Cat" },
+{ "5b43575a86f77424f443fe62", "Fcond" },
+{ "59e3577886f774176a362503", "Sugar"},
+{ "5c0e531286f7747fa54205c2", "SJ1 TGLabs" },
+{ "5c0e531d86f7747fa23f4d42", "SJ6 TGLabs" },
+{ "5af0534a86f7743b6f354284", "Ophthalmoscope"},
+{ "5d1b385e86f774252167b98a", "Filter"},
+{ "5d02778e86f774203e7dedbe", "CMS" },
+{ "5d02797c86f774203f38e30a", "Surv12" },
+{ "590de71386f774347051a052", "Teapot" },
+{ "590de7e986f7741b096e5f32", "Vase" },
+
+//Munition
+{ "5c1262a286f7743f8a69aab2", "7N39" },
+{ "5c0d5e4486f77478390952fe", "7N39" },
+{ "59e690b686f7746c9f75e848", "M995" },
+};
---
+++ src/questitems.hpp
@@ -0,0 +1,15 @@
+std::unordered_map<std::string, std::string> questitems = {
+
+   //Quest Items
+{ "590c621186f774138d11ea29", "Flash drive"}, 
+{ "59e763f286f7742ee57895da", "Pilgrim"},
+
+   // Road to Kappa Container
+{ "5bc9bdb8d4351e003562b8a1", "Badge" },
+{ "5bc9c377d4351e3bac12251b", "Firesteel" },
+
+//KEK
+//Cordura
+//Ripstop
+
+};
